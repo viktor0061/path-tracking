3baa3a982040ffc900d5157f513f305e55c7cf84
viktor-csanak <viktor.csanak@icbtech.rs>
2023-05-07 13:24:14 +0200

    Store and draw tracks to frames

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import ultralytics
-import sys
 from bytetracker.byte_tracker import BYTETracker, STrack
 from onemetric.cv.utils.iou import box_iou_batch
-from dataclasses import dataclass
-import supervision
-from supervision import ColorPalette
 from supervision import Point
 from supervision import VideoInfo
 from supervision import get_video_frames_generator
 from supervision import VideoSink
-from supervision import show_frame_in_notebook
 from supervision import Detections, BoxAnnotator
 from supervision.detection.line_counter import LineZone, LineZoneAnnotator
+
+from tqdm import tqdm
 
 import cv2
 from typing import List
@@ -20,18 +16,20 @@ import numpy as np
 
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-#Determine the input video
+#Input and output video path
 SOURCE_VIDEO_PATH = "traffic-detection.mp4"
+TARGET_VIDEO_PATH = "traffic-detection-result.mp4"
 
 #Define BYTE parameters
 #Detection score threshold
-BYTETRACKER_TRACK_TRESH = 0.25
+BYTETRACK_DETECTION_TRESH = 0.45
 #Matching algorithm(i.e. IoU/Re-ID) threshold
-BYTETRACKER_MATCH_TRESH = 0.4
+BYTETRACK_MATCH_TRESH = 0.8
 #Track buffer and frame rate will be used to determine the time after a lost track will be marked removed
-BYTETRACKER_TRACK_BUFFER = 30
-BYTETRACKER_FRAME_RATE = 30
+BYTETRACK_TRACK_BUFFER = 30
+BYTETRACK_FRAME_RATE = 30
     
 #Converts Detections into format that can be consumed by ByteTracker.update()
 def detections2boxes(detections: Detections) -> np.ndarray:
@@ -79,36 +77,31 @@ CLASS_NAMES_DICT = model.model.names
 #Class_ids of interest - car, motorcycle, bus and truck
 CLASS_ID = [2, 3, 5, 7]
 
-#Settings
-LINE_START = Point(50, 1500)
-LINE_END = Point(3840-50, 1500)
-
-#Output video path
-TARGET_VIDEO_PATH = "traffic-detection-result.mp4"
-
 #Create BYTETracker instance
-byte_tracker = BYTETracker(track_thresh=BYTETRACKER_TRACK_TRESH, 
-                           track_buffer=BYTETRACKER_TRACK_BUFFER, 
-                           match_thresh=BYTETRACKER_MATCH_TRESH, 
-                           frame_rate=BYTETRACKER_FRAME_RATE)
+byte_tracker = BYTETracker(track_thresh=BYTETRACK_DETECTION_TRESH, 
+                           track_buffer=BYTETRACK_TRACK_BUFFER, 
+                           match_thresh=BYTETRACK_MATCH_TRESH, 
+                           frame_rate=BYTETRACK_FRAME_RATE)
 
 #Create VideoInfo instance
 video_info = VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
 #Create frame generator
 generator = get_video_frames_generator(SOURCE_VIDEO_PATH)
-#Create LineCounter instance
-line_counter = LineZone(start=LINE_START, end=LINE_END)
-#Create instance of BoxAnnotator and LineCounterAnnotator
-box_annotator = BoxAnnotator(thickness=4, text_thickness=4, text_scale=2)
-line_annotator = LineZoneAnnotator(thickness=4, text_thickness=4, text_scale=2)
+#Create instance of BoxAnnotator
+#box_annotator = BoxAnnotator(thickness=4, text_thickness=4, text_scale=2)
 
 #Acquire first video frame
 iterator = iter(generator)
 
+#Initiate a progress bar
+progress_bar = tqdm(total=video_info.total_frames, dynamic_ncols=True, position=0, leave=True)
+
 with VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
+    #initialise dictionary for tracks
+    paths = {}
+
     #Loop over video frames
     for frame_number in range (video_info.total_frames):
-
         frame = next(iterator)
         #Detection on single frame
         results = model(frame)
@@ -120,9 +113,11 @@ with VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
         mask = np.array([class_id in CLASS_ID for class_id in detections.class_id], dtype=bool)
         detections.filter(mask=mask, inplace=True)
 
+        detection_array = detections2boxes(detections=detections)
+
         #Update tracks with detections
         tracks = byte_tracker.update(
-            dets=detections2boxes(detections=detections), _=frame.shape
+            dets=detection_array, _=frame.shape
         )
 
         #Match the returned tracks with the input detection
@@ -133,23 +128,40 @@ with VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
         mask = np.array([tracker_id is not None for tracker_id in detections.tracker_id], dtype=bool)
         detections.filter(mask=mask, inplace=True)
 
-        #Format custom labels
-        labels = [
-            f"{tracker_id} {CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
-            for _, confidence, class_id, tracker_id
-            in detections
-        ]
+        #Append new tracks to paths dictionary, assign a colour and add data points
+        for xyxy, confidence, class_id, tracker_id in detections:
+            if tracker_id not in paths:
+                colour = (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255))
+                paths[tracker_id] = {'colour':colour, 'points':[]}
 
-        # Updating line counter
-        line_counter.trigger(detections=detections)
-        #print(line_counter.in_count)
-        #print(line_counter.out_count)
+            x, y = (xyxy[0] + xyxy[2]) / 2, (xyxy[1] + xyxy[3]) / 2
+
+            paths[tracker_id]['points'].append((x, y))
+
+        #Draw the paths to the frame
+        for path in paths.values():
+            for point in path['points']:
+                cv2.drawMarker(img=frame, 
+                            position=(int(point[0]), int(point[1])), 
+                            color=path['colour'], 
+                            markerType=cv2.MARKER_STAR)
+
+        #Format custom labels
+        #labels = [
+        #    f"{tracker_id} {CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
+        #    for _, confidence, class_id, tracker_id
+        #    in detections
+        #]
 
         #Annotate and display frame
-        frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
-        line_annotator.annotate(frame=frame, line_counter=line_counter)
+        #frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
+
+        #Write frame to output
         sink.write_frame(frame)
-        #cv2.imshow("IMG",frame)
+
+        #update progress bar
+        progress_bar.update(1)
+
         if cv2.waitKey(1)&0xFF==27:
             break
 
